e6eb82b26722b64db07a4589b7aba7eeaf57d30d
Louis Vignoli <louis.vignoli@pasqal.com>
2022-07-13 12:07:40 +0200

    simplify demo, adds number of *available* seats and cry

--- main.rs
+++ main.rs
@@ -1,30 +1,31 @@
 use error_chain::error_chain;
 use serde_json::Value;
-use std::io::Read;
 
 error_chain! {
     foreign_links {
         Io(std::io::Error);
         HttpRequest(reqwest::Error);
+        JSON(serde_json::Error);
+    }
+}
+const PARIS_MARSEILLE_URL: &str = "https://data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&facet=date&facet=origine&facet=od_happy_card&facet=destination&refine.origine=PARIS+(intramuros)&refine.destination=MARSEILLE+ST+CHARLES";
+const PARIS_MARSEILLE_HAPPY_CARD_URL: &str = "https://data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&facet=date&facet=origine&facet=destination&facet=od_happy_card&refine.origine=PARIS+(intramuros)&refine.destination=MARSEILLE+ST+CHARLES&refine.od_happy_card=OUI";
+
+fn main() {
+    // The MAX JEUNE URL with query strings Paris => Marseille.
+    // It is a public endpoint so no token needed.
+
+    if let Ok(n_max_jeune) = get_number_from_url(PARIS_MARSEILLE_URL) {
+        println!("Number of MAX JEUNE: {}.", n_max_jeune)
+    }
+
+    if let Ok(n_max_jeune_available) = get_number_from_url(PARIS_MARSEILLE_HAPPY_CARD_URL) {
+        println!("Number of available MAX JEUNE: {}.", n_max_jeune_available)
     }
 }
 
-fn main() -> Result<()> {
-    // The MAX JEUNE URL with query string Paris => Marseille.
-    // It is a public endpoint so no token needed.
-    let url = "https://data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&facet=date&facet=origine&facet=od_happy_card&facet=destination&refine.origine=PARIS+(intramuros)&refine.destination=MARSEILLE+ST+CHARLES";
-    let mut res = reqwest::blocking::get(url)?;
-    let mut body = String::new();
-    res.read_to_string(&mut body)?;
-
-    process(body);
-
-    Ok(())
-}
-
-/// Processes the response body.
-fn process(s: String) -> serde_json::Result<()> {
-    let v: Value = serde_json::from_str(s.as_str())?;
-    println!("Number of MAX JEUNE from PARIS (intramuros) to MARSEILLE SAINT CHARLES in the next 30 days: {}.", v["nhits"]);
-    Ok(())
+fn get_number_from_url(url: &str) -> Result<u64> {
+    let body = reqwest::blocking::get(url)?.text()?;
+    let v: Value = serde_json::from_str(&body)?;
+    Ok(v["nhits"].as_u64().ok_or("cannot convert")?)
 }
